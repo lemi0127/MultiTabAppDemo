4dd84653b5a5c08e32add3e8cca7dc8b9c1d4ee6
Alice Lee <lemi0127@algonquinlive.com>
2015-10-06 18:49:07 -0400

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,94 @@
+angular.module('starter.controllers', [])
+
+.controller('TodoHomeCtrl',['$scope', '$localStorage', function($scope,$localStorage) {
+    $scope.todos = {
+        text: ' '
+    };
+
+    if($localStorage.todos){
+        $scope.todos = $localStorage.todos;
+    } else {
+        $scope.todos = [
+        {text:"Task 1", done: false},
+        {text:"Task 2", done: false},
+        {text:"Task 3", done: false}
+                        ]
+    }
+    
+    $scope.addTodo = function () {
+        $scope.todos.push({
+            'text': $scope.todoText,
+            'done': false
+        });
+        $localStorage.todos = $scope.todos;
+        $scope.todoText = '';
+    }
+    
+   $scope.clear = function (index) {
+        delete $scope.todos[index];
+        $localStorage.todos = $scope.todos;
+    }
+    
+}])
+
+.controller('TodoList2Ctrl',['$scope', '$localStorage', function($scope,$localStorage) {
+    $scope.todos = {
+        text: ' '
+    };
+
+    if($localStorage.todos){
+        $scope.todos = $localStorage.todos;
+    } else {
+        $scope.todos = [
+        {text:"Task 1", done: false},
+        {text:"Task 2", done: false},
+        {text:"Task 3", done: false}
+                        ]
+    }
+    
+    $scope.addTodo = function () {
+        $scope.todos.push({
+            'text': $scope.todoText,
+            'done': false
+        });
+        $localStorage.todos = $scope.todos;
+        $scope.todoText = '';
+    }
+    
+   $scope.clear = function (index) {
+        delete $scope.todos[index];
+        $localStorage.todos = $scope.todos;
+    }
+    
+}])
+
+.controller('TodoList3Ctrl',['$scope', '$localStorage', function($scope,$localStorage) {
+    $scope.todos = {
+        text: ' '
+    };
+
+    if($localStorage.todos){
+        $scope.todos = $localStorage.todos;
+    } else {
+        $scope.todos = [
+        {text:"Task 1", done: false},
+        {text:"Task 2", done: false},
+        {text:"Task 3", done: false}
+                        ]
+    }
+    
+    $scope.addTodo = function () {
+        $scope.todos.push({
+            'text': $scope.todoText,
+            'done': false
+        });
+        $localStorage.todos = $scope.todos;
+        $scope.todoText = '';
+    }
+    
+   $scope.clear = function (index) {
+        delete $scope.todos[index];
+        $localStorage.todos = $scope.todos;
+    }
+    
+}]);
